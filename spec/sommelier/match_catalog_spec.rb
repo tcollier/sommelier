ba021a2bb92395eba75e2ce821b1198d0c6280d5
Tom Collier <collier@apartmentlist.com>
2018-07-21 09:56:44 -0700

    Add MatchCatalog specs

--- spec/sommelier/match_catalog_spec.rb
+++ spec/sommelier/match_catalog_spec.rb
@@ -2,6 +2,44 @@ require 'sommelier/match_catalog'
 
 class Sommelier
   describe MatchCatalog do
+    describe '#each_dish' do
+      subject(:catalog) { described_class.new }
+
+      before do
+        catalog.add('A', 'X', 2)
+        catalog.add('A', 'Y', 4)
+        catalog.add('A', 'Z', 3)
+        catalog.add('B', 'Z', 9)
+      end
+
+      it 'yields each dish along with the number of wines it is matched with' do
+        found = {}
+        catalog.each_dish do |dish, preferences_count|
+          found[dish] = preferences_count
+        end
+        expect(found.length).to eq(2)
+        expect(found).to have_key('A')
+        expect(found).to have_key('B')
+        expect(found['A']).to eq(3)
+        expect(found['B']).to eq(1)
+      end
+    end
+
+    describe '#wine_preferred_at' do
+      subject(:catalog) { described_class.new }
+
+      before do
+        catalog.add('A', 'X', 2)
+        catalog.add('A', 'Y', 4)
+        catalog.add('A', 'Z', 3)
+        catalog.add('B', 'Z', 9)
+      end
+
+      it 'returns the Nth preferred wine for the dish' do
+        expect(catalog.wine_preferred_at('A', 2)).to eq('X')
+      end
+    end
+
     describe '#most_preferred_dish' do
       subject(:catalog) { described_class.new }
 
