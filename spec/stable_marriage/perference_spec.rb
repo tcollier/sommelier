e887650837438cd9afce8dda1a50e52adf0d4f88
Tom Collier <collier@apartmentlist.com>
2018-07-21 00:02:43 -0700

    Add spec for Preference

---
+++ spec/stable_marriage/perference_spec.rb
@@ -0,0 +1,31 @@
+class StableMarriage
+  describe Preference do
+    describe '#>' do
+      subject(:preference) { described_class.new('Foo', 10) }
+
+      context 'when subject has greater score than other' do
+        let(:other) { described_class.new('Bar', 9) }
+
+        specify do
+          expect(preference > other).to eq(true)
+        end
+      end
+
+      context 'when subject has score equal to other' do
+        let(:other) { described_class.new('Bar', 10) }
+
+        specify do
+          expect(preference > other).to eq(false)
+        end
+      end
+
+      context 'when subject has lesser score than other' do
+        let(:other) { described_class.new('Bar', 11) }
+
+        specify do
+          expect(preference > other).to eq(false)
+        end
+      end
+    end
+  end
+end
